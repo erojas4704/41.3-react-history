11b88fae832c3c74eb44e9e5002419b7f85e3f6e
erojas4704 <55060308+erojas4704@users.noreply.github.com>
2021-07-31 15:47:05 -0400

    initial

---
+++ src/Joke.js
@@ -0,0 +1,34 @@
+import React from "react";
+import "./Joke.css";
+
+class Joke extends React.Component {
+  constructor(props) {
+    super(props);
+    const { vote, id } = props;
+    this.vote = vote;
+    this.id = id;
+  }
+
+  upVote = () => this.vote(this.id, +1);
+  downVote = () => this.vote(this.id, -1);
+
+  render = () => (
+    <div className = "Joke" >
+      <div className="Joke-votearea">
+        <button onClick={this.upVote}>
+          <i className="fas fa-thumbs-up" />
+        </button>
+
+        <button onClick={this.downVote}>
+          <i className="fas fa-thumbs-down" />
+        </button>
+
+        {this.props.votes}
+      </div>
+
+      <div className="Joke-text">{this.props.text}</div>
+    </div>
+  );
+}
+
+export default Joke;
